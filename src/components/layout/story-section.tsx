ce8ad1f3f71a9d6f182bc75ec6f2cf8ce33163e7
Haseeb Ahmed Raza Khan <web.dev.haseeb@gmail.com>
2025-02-21 22:42:26 +0500

    3 player video done

--- src/components/layout/story-section.tsx
+++ src/components/layout/story-section.tsx
@@ -8,8 +8,8 @@ const Stories = [
             achievement: "98 runs",
             when: "Against Pakistan, 2003 World Cup",
             description: "Sachin's 98 Against Pakistan, 2003 World Cup",
-            image: "banner.jpeg",
-            link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            image: "https://data1.ibtimes.co.in/en/full/599672/sachin-tendulkar.jpg",
+            link: "https://www.facebook.com/watch/?v=2584290218392691",
         },
         {
             id: "sachin-2", 
@@ -17,8 +17,8 @@ const Stories = [
             achievement: "Unbeaten 241*",
             when: "In Sydney, 2004",
             description: "Sachin's Unbeaten 241* in Sydney, 2004",
-            image: "banner.jpeg",
-            link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            image: "https://sc0.blr1.cdn.digitaloceanspaces.com/article/142178-vzhksconcu-1590917337.jpg",
+            link: "https://www.youtube.com/watch?v=uOA25BRgSic",
         },
         {
             id: "sachin-3",
@@ -26,8 +26,8 @@ const Stories = [
             achievement: "200 runs",
             when: "In India vs South Africa, 2nd ODI, 2010",
             description: "Sachin's 200 in India vs South Africa, 2nd ODI, 2010",
-            image: "banner.jpeg",
-            link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            image: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202202/sachin200_1200x768.jpeg?size=690:388",
+            link: "https://www.youtube.com/watch?v=OzfdMB2bVuA",
         },
         {
             id: "sachin-4",
@@ -35,8 +35,8 @@ const Stories = [
             achievement: "186* runs",
             when: "VS New Zealand, 1999",
             description: "Sachin Tendulkar's 186* vs New Zealand, 1999",
-            image: "banner.jpeg",
-            link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            image: "https://scontent.fknu1-2.fna.fbcdn.net/v/t1.6435-9/124045290_3939095289442871_5962850973004373788_n.jpg?stp=dst-jpg_p180x540_tt6&_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_ohc=yf5HBslJwm8Q7kNvgGIe0Si&_nc_oc=AdgEbLsaydFf4w5Ga7-vPXzH2rJZtHJaInur5do1c9Dq91YwssdjaIhV2r_kexY1r1bkHZ9EQmQkxU82Xzuw066p&_nc_zt=23&_nc_ht=scontent.fknu1-2.fna&_nc_gid=Ai2_p9mZ2sjhyRNXfn_AeJc&oh=00_AYBRrzIbLnmRAlwt52BIYW9Bu0RTK5oImaVHKcDHbf9ECA&oe=67E0250A",
+            link: "https://www.youtube.com/watch?v=cv0OE3wYRIg",
         },
         {
             id: "sachin-5",
@@ -44,8 +44,8 @@ const Stories = [
             achievement: "119* runs",
             when: "VS England 1991",
             description: "Sachin's first century ever - 119* vs England 1991",
-            image: "banner.jpeg",
-            link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            image: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202408/sachin-tendulkar-144726942-16x9_0.jpg?VersionId=gRtUFNvZKrUp0t4qGMg4L7sTKOP46vZz&size=690:388",
+            link: "https://www.youtube.com/watch?v=G27C4AHk-Iw",
         }
     ],
     [
@@ -148,37 +148,19 @@ const Stories = [
             name: "Irfan Pathan",
             achievement: "Hat-trick",
             when: "VS Pakistan",
-            description: "Pathan's Hatrick against Pakistan",
-            image: "banner.jpeg",
-            link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            description: "Pathan's Hatrick against Pakistan", 
+            image: "/Hatrick-against-Pakistan.jpg",
+            link: "https://www.youtube.com/watch?v=WJXAXt8KP4U",
         },
         {
-            id: "pathan-2",
+            id: "pathan-2", 
             name: "Irfan Pathan",
             achievement: "Player of the Match",
             when: "T20 World Cup final 2007",
             description: "Player-of-the-Match in 2007 T20 World Cup final",
-            image: "banner.jpeg",
-            link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            image: "https://dxn76xj9mnmqa.cloudfront.net/images/Irfan-Pathan_t20_world_cup_2007.width-800.jpg",
+            link: "https://fb.watch/xUsDyV6wLg/",
         },
-        {
-            id: "pathan-3",
-            name: "Irfan Pathan",
-            achievement: "5 wickets",
-            when: "VS Pakistan 2006",
-            description: "Fifer against Pakistan 2006",
-            image: "banner.jpeg",
-            link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-        },
-        {
-            id: "pathan-4",
-            name: "Irfan Pathan",
-            achievement: "First Test century",
-            when: "2007",
-            description: "Maiden Test century in 2007",
-            image: "banner.jpeg",
-            link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-        }
     ]
 ];
 
@@ -197,7 +179,7 @@ export default function StorySection() {
                     <a key={story.id} href={story.link} className="relative flex flex-col items-center justify-center border-border border-2 rounded-lg overflow-hidden hover:[&>div]:opacity-100">
                         <img src={story.image} alt={story.description} className="md:h-60 h-52  rounded-lg w-auto" />
                         <div className="absolute inset-0 bg-gradient-to-br from-blue-600/90 to-orange-500/50 opacity-0 transition-opacity duration-300" />
-                        <h1 className="absolute bottom-2 left-4 uppercase text-pretty text-white text-2xl font-bold [text-shadow:_2px_2px_0_rgb(0_0_0_/_40%)]">{story.description}</h1>
+                        {/* <h1 className="absolute bottom-2 left-4 uppercase text-pretty text-white text-2xl font-bold [text-shadow:_2px_2px_0_rgb(0_0_0_/_40%)]">{story.description}</h1> */}
                     </a>
                 ))}
             </Marquee>
@@ -211,7 +193,35 @@ export default function StorySection() {
                     <a key={story.id} href={story.link} className="relative flex flex-col items-center justify-center border-border border-2 rounded-lg overflow-hidden hover:[&>div]:opacity-100">
                         <img src={story.image} alt={story.description} className="md:h-60 h-52  rounded-lg w-auto" />
                         <div className="absolute inset-0 bg-gradient-to-br from-blue-600/70 to-orange-500/30 opacity-0 transition-opacity duration-300" />
-                        <h1 className="absolute bottom-2 left-4 uppercase text-pretty text-white text-2xl font-bold [text-shadow:_2px_2px_0_rgb(0_0_0_/_40%)]">{story.description}</h1>
+                        {/* <h1 className="absolute bottom-2 left-4 uppercase text-pretty text-white text-2xl font-bold [text-shadow:_2px_2px_0_rgb(0_0_0_/_40%)]">{story.description}</h1> */}
+                    </a>
+                ))}
+            </Marquee>
+            <Marquee 
+                className="space-x-0 [--duration:15s]"
+                repeat={10}
+                pauseOnHover={true}
+                reverse={true}
+            >
+                {Stories[2].map((story) => (
+                    <a key={story.id} href={story.link} className="relative flex flex-col items-center justify-center border-border border-2 rounded-lg overflow-hidden hover:[&>div]:opacity-100">
+                        <img src={story.image} alt={story.description} className="md:h-60 h-52  rounded-lg w-auto" />
+                        <div className="absolute inset-0 bg-gradient-to-br from-blue-600/70 to-orange-500/30 opacity-0 transition-opacity duration-300" />
+                        {/* <h1 className="absolute bottom-2 left-4 uppercase text-pretty text-white text-2xl font-bold [text-shadow:_2px_2px_0_rgb(0_0_0_/_40%)]">{story.description}</h1> */}
+                    </a>
+                ))}
+            </Marquee>
+            <Marquee 
+                className="space-x-0 [--duration:15s]"
+                repeat={10}
+                pauseOnHover={true}
+                reverse={true}
+            >
+                {Stories[3].map((story) => (
+                    <a key={story.id} href={story.link} className="relative flex flex-col items-center justify-center border-border border-2 rounded-lg overflow-hidden hover:[&>div]:opacity-100">
+                        <img src={story.image} alt={story.description} className="md:h-60 h-52  rounded-lg w-auto" />
+                        <div className="absolute inset-0 bg-gradient-to-br from-blue-600/70 to-orange-500/30 opacity-0 transition-opacity duration-300" />
+                        {/* <h1 className="absolute bottom-2 left-4 uppercase text-pretty text-white text-2xl font-bold [text-shadow:_2px_2px_0_rgb(0_0_0_/_40%)]">{story.description}</h1> */}
                     </a>
                 ))}
             </Marquee>
